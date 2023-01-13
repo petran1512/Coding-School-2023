5c6340269fc5b94366296195cb3c0ce085f135b5
petran15 <kainourgiogiapame15@gmail.com>
2023-01-13 14:26:52 +0200

    commit Session-05-Exone

---
+++ Session-05/Session-05/Exone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    public class ExersiceOne
+    {
+        //Write a C# program that reverses a given string (your name).
+        public string Reverse()
+        {
+            Console.WriteLine("Enter a String: ");
+            string name = Console.ReadLine();
+            string reversed = string.Empty;
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversed += name[i];
+            }
+            return reversed;
+        }
+    }
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,7 @@
+﻿using Session_04;
+{
+    Console.WriteLine("First Exersice:"); ExersiceOne exersiceone = new ExersiceOne();
+    Console.Write(exersiceone.Reverse()); Console.WriteLine();
+
+    Console.ReadLine();
+}
