456b0405f98daaf893c970d7f571492524a27ba3
petran15 <kainourgiogiapame15@gmail.com>
2023-01-13 16:30:22 +0200

    commit extwo

---
+++ Session-05/Session-05/Extwo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Session_05
+{
+    public class ExersiceTwo
+    {
+        /*Write a C# program that asks the user for an integer (n) and gives them
+        the possibility to choose between computing the sum and computing the product of 1,...,n.*/
+        public int Choose()
+        {
+            int sum = 0;
+            int product = 1;
+            Console.WriteLine("Enter a Integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 1 for computhing sum or 2 for computing product: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    sum += i;
+                }
+            }
+            if (choice == 2) {
+                for (int i = 1; i <= n; i++)
+                {
+                    product *= i;
+                }
+                //return product;
+            }
+            return sum;
+            return product;
+        }
+    }
+}
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Session_05;
 {
     Console.WriteLine("First Exersice:"); ExersiceOne exersiceone = new ExersiceOne();
-    Console.Write(exersiceone.Reverse()); Console.WriteLine();
+    Console.Write(exersiceone.Reverse()); Console.WriteLine("\n");
+
+    Console.WriteLine("Second Exersice:"); ExersiceTwo exersicetwo = new ExersiceTwo();
+    Console.Write(exersicetwo.Choose()); Console.WriteLine();
+
 
     Console.ReadLine();
 }
