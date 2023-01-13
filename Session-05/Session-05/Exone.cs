b6ce854d95164e3365a478856ff84aa39f597f79
petran15 <kainourgiogiapame15@gmail.com>
2023-01-13 14:28:48 +0200

    commit Session-05-Exone-1

--- Session-05/Session-05/Exone.cs
+++ Session-05/Session-05/Exone.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Session_04
+namespace Session_05
 {
     public class ExersiceOne
     {
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -1,4 +1,4 @@
-﻿using Session_04;
+﻿using Session_05;
 {
     Console.WriteLine("First Exersice:"); ExersiceOne exersiceone = new ExersiceOne();
     Console.Write(exersiceone.Reverse()); Console.WriteLine();
