64174b2b755a7923b342049eb8b4cd713bd82bfb
petran15 <kainourgiogiapame15@gmail.com>
2023-01-14 10:39:09 +0200

    commit changes 0

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -6,12 +6,5 @@
     Console.WriteLine("Second Exersice:"); ExersiceTwo exersicetwo = new ExersiceTwo();
     Console.Write(exersicetwo.Choose()); Console.WriteLine("\n");
 
-    Console.WriteLine("Third Exersice:"); ExersiceThree exersicethree = new ExersiceThree();
-    Console.Write(exersicethree.Prime()); Console.WriteLine("\n");
-
-    Console.WriteLine("Fourth Exersice:"); ExersiceFour exersicefour = new ExersiceFour();
-    Console.Write(exersicefour.Multiply()); Console.WriteLine(" ");
-
-
     Console.ReadLine();
 }
