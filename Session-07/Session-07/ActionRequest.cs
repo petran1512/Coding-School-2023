06a428b794d6420c5b3f2cc667cf7e4d1e1645d3
petran15 <kainourgiogiapame15@gmail.com>
2023-01-18 13:35:02 +0200

    commit session-07 ActReq

--- Session-07/Session-07/ActionRequest.cs
+++ Session-07/Session-07/ActionRequest.cs
@@ -7,21 +7,29 @@ using System.Threading.Tasks;
 
 namespace Session_07
 {
-    public class ActionRequest : Action
-    {
+
+        public class ActionEntity
+        {
+            public Guid RequestID { get; set; }
+        }
         public enum ActionEnum
         {
             Convert,
             Uppercase,
             Reverse
         }
-
+    public class ActionRequest : ActionEntity
+    {
 
         //props
-        public Guid RequestID { get; set; }
         public string Input { get; set; }
 
         public ActionEnum Action { get; set; }
+
+        public ActionRequest() {
+            RequestID = Guid.NewGuid();
+
+        }
 
         //consts
         public ActionRequest()
