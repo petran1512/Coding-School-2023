0e62c666e4782be10b1df32c9dd76ceddab12aa6
petran15 <kainourgiogiapame15@gmail.com>
2023-01-19 14:37:35 +0200

    commit changes all buttons

--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -6,5 +6,25 @@ namespace Session_09
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
