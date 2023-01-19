ba6e5db594649c262681ce4fa398f1762ba9db8a
petran15 <kainourgiogiapame15@gmail.com>
2023-01-19 18:02:39 +0200

    commit

--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -121,31 +121,136 @@ namespace Session_09
 
         private void btn4_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
 
+            ctrlDisplay.Text += " 4 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 4;
+            }
+            else
+            {
+                _value2 = 4;
+            }
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
 
+            ctrlDisplay.Text += " 5 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 5;
+            }
+            else
+            {
+                _value2 = 5;
+            }
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
 
+            ctrlDisplay.Text += " 6 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 6;
+            }
+            else
+            {
+                _value2 = 6;
+            }
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+
+            ctrlDisplay.Text += " 7 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 7;
+            }
+            else
+            {
+                _value2 = 7;
+            }
 
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+
+            ctrlDisplay.Text += " 8 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 8;
+            }
+            else
+            {
+                _value2 = 8;
+            }
 
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+
+            ctrlDisplay.Text += " 9 ";
+
+            if (_value1 == null)
+            {
+                _value1 = 9;
+            }
+            else
+            {
+                _value2 = 9;
+            }
 
         }
 
