7010d95ed6004da46fe25096b50fc38249c0f774
petran15 <kainourgiogiapame15@gmail.com>
2023-01-31 17:51:58 +0200

    commit customers

---
+++ Session-16/EF.Course.Orm/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Course.Orm.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<CustomerConfiguration>
+    {
+        public void Configure(EntityTypeBuilder<CustomerConfiguration> builder)
+        {
+            builder.ToTable("Customer");
+
+            //builder.Property(customer => customer.Phone).HasMaxLength(10).IsRequired(true);
+
+            //builder.HasKey(customer => customer.TIN);
+        }
+    }
+}
+
