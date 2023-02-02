930095da72a0c912ba41ea39b895ec90a042e78a
petran15 <kainourgiogiapame15@gmail.com>
2023-02-02 18:22:07 +0200

    commit

---
+++ Session-16/EF.Petshop.Model/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Course.Model
+{
+    public class Todo
+    {
+        public Todo(string title)
+        {
+            Title = title;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Finished { get; set; }
+    }
+}
