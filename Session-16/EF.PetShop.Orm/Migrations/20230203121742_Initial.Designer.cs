c652f2648fcdb19c93039bb83e6ef6261d00c642
petran15 <kainourgiogiapame15@gmail.com>
2023-02-03 14:18:43 +0200

    commit

--- Session-16/EF.PetShop.Orm/Migrations/20230203121742_Initial.Designer.cs
+++ Session-16/EF.PetShop.Orm/Migrations/20230203121742_Initial.Designer.cs
@@ -4,6 +4,7 @@ using EF.Course.Orm.AppDBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF.Petshop.Orm.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230203121742_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
