7567a1439ec4104561e9455d29845a8a7d3f9b59
petran15 <kainourgiogiapame15@gmail.com>
2023-02-07 15:15:18 +0200

    commit changes 1,2

--- Session-22/Session-22.js
+++ Session-22/Session-22.js
@@ -2,9 +2,28 @@ let reverseBtn = document.querySelector("#reverseBtn");
 let inputReverse = document.querySelector("#inputReverse");
 let resultReverse = document.querySelector("#outputReverse");
 
-reverseBtn.addEventListener("click", reverseString(inputReverse.Value));
-
+reverseBtn.addEventListener("click", reverseStr);
+function reverseStr(){
+    resultReverse.textContent = reverseString(inputReverse.value);
+}
 
 function reverseString(str){
-    resultReverse.textContent = str.split("").reverse().join("");
+    return str.split("").reverse().join("");
 }
+
+let palindromeBtn = document.querySelector("#palindromeBtn");
+let inputPalindrome = document.querySelector("#inputpalindrome");
+let resultPalindrome = document.querySelector("#outputpalindrome");
+
+
+palindromeBtn.addEventListener("click", ispalindrome);
+
+function ispalindrome(){
+    let str = inputPalindrome.value;
+    let reversedStr = reverseString(str);
+    if (str === reversedStr)
+        resultPalindrome.textContent = "True";
+    else
+        resultPalindrome.textContent = "False";
+}
+
