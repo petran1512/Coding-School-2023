60abec09c442561fb52226bd5826985351f6516a
petran15 <kainourgiogiapame15@gmail.com>
2023-02-09 18:09:55 +0200

    commit

--- Session-23/PetShop.EF/Repositories/EmployeeRepo.cs
+++ Session-23/PetShop.EF/Repositories/EmployeeRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace PetShop.EF.Repositories
 {
-    internal class EmployeeRepo
+    internal class EmployeeRepo : IEntityRepo<Employee>
     {
+        public void Add(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Employee entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
